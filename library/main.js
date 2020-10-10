49c3904ee290d05ab412e27a86724624cc60b3a9
Andrea Maiolo <maiolo.m.2@gmail.com>
2020-10-10 22:41:57 +0200

    first work on library

--- library/main.js
+++ library/main.js
@@ -7,6 +7,23 @@ function addBooks(title,author,pages,read){
     library.push(myBook);
 }
 
+//display the library
+const display = document.querySelector('.shelf');
+const page = document.createElement('p');
+var b = document.createElement('p');
+function showMe(library) {
+    for(let i=0;i<library.length;i++){
+        b.textContent = library[i].info();
+        display.appendChild(b)
+    }
+    
+}
+// Add a “NEW BOOK” button that brings up a form allowing users to input the details for the 
+// new book: author, title, number of pages, whether it’s been read and anything else you might want.
+
+const buttonNewBook =document.querySelector('#addNewBook');
+const form = document.querySelector('#form');
+buttonNewBook.addEventListener('click', () => form.style.display('inline-block') )
 
 //this is the construct for the books
 function Book(title, author, pages, read){
