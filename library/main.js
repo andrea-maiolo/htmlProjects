6a405061a677293cd1d82a1bc815ba524c93d7f1
Andrea Maiolo <maiolo.m.2@gmail.com>
2020-10-11 18:18:32 +0200

    new books added to library and show on shelf

--- library/main.js
+++ library/main.js
@@ -1,45 +1,66 @@
-//this is going to be y library
+//this is going to be our library
 let library = [];
 
-//this function will add books to the library
-function addBooks(title,author,pages,read){
-    let myBook = new Book(title,author,pages, read);
-    library.push(myBook);
-}
-
-//display the library
-const add = document.getElementById('+');
-// add.addEventListener('click', formCheck);
-const title = document.getElementById('title');
-const author = document.getElementById('author');
-const pages = document.getElementById('pages');
+//variabels form form 
+const title = document.querySelector('#title');
+const author = document.querySelector('#author');
+const pages = document.querySelector('#pages');
+//this is for the display so we can add books on the shelf
+const display = document.querySelector('#display')
 //radio buttons
-const read = document.getElementById('read'); 
-const reading = document.getElementById('reading');
-const toRead = document.getElementById('to read');
-read.addEventListener('click', () => {console.log("hello")})
-// function formCheck(){
-//     // if((title.value != '' && author.value != '' && pages.value != '') && (read.value != ''|| reading.value!='' || toRead.value != '')){
-//         console.log("hello")
-//     }
-// }
-const display = document.querySelector('.shelf');
-const page = document.createElement('p');
-var b = document.createElement('p');
-function showMe(library) {
-    for(let i=0;i<library.length;i++){
-        b.textContent = library[i].info();
-        display.appendChild(b)
-    }
-    
-}
-// Add a “NEW BOOK” button that brings up a form allowing users to input the details for the 
-// new book: author, title, number of pages, whether it’s been read and anything else you might want.
-const buttonNewBook =document.querySelector('#addNewBook');
-const form = document.querySelector('#form');
-buttonNewBook.addEventListener('click', () => formAddingBook.style.display="block");
+const statusRead = document.querySelector('#statusRead');
+const statusToRead = document.querySelector('#statusToRead');
+//this show the form when clicked
+const formAddingBook = document.querySelector('#formAddingBook');
+//this is to show the form for adding a book
+const addNewBook = document.querySelector('#addNewBook');
+addNewBook.addEventListener('click', ()=>{formAddingBook.style.display ="block"; adding.style.display = "block"})
+//this button will actually add the books on the shelf
+const adding = document.querySelector('#adding');
+adding.addEventListener('click',addingToLibrary);
+//this function append the books as p elements on the screen
+function addingToLibrary(){
+    if((title.value!= '' && author.value!= ''&& pages.value!= '') && (statusRead.checked || statusToRead.checked)){
+        if(statusRead.checked){
+            var stat = true;
+        }else if(statusToRead.checked){
+            stat = false;
+        }  
+    let newB = new Book(title.value, author.value, pages.value,stat );
+    library.push(newB)
+    showMe(newB)
+    }}
 
-//this is the construct for the books
+//show me the library
+function showMe(element){
+    let myBook =document.createElement('div');
+    myBook.classList.add("books")
+    let myBookTitle =document.createElement('p');
+    myBookTitle.innerHTML = element.title;
+    let myBookAuthor =document.createElement('p');
+    myBookAuthor.innerHTML = element.author;
+    let myBookPages =document.createElement('p');
+    myBookPages.innerHTML = element.pages;
+    let myBookStatus = document.createElement('p');
+    myBookStatus.innerHTML = element.info();
+    myBook.appendChild(myBookTitle);
+    myBook.appendChild(myBookAuthor);
+    myBook.appendChild(myBookPages);
+    myBook.appendChild(myBookStatus);
+    // myBook.appendChild(myBookStatus);
+    display.appendChild(myBook);    
+
+    
+    // let myBook = document.createElement('p');
+    // myBook.innerHTML = element.info();
+    // display.appendChild(myBook)
+}
+
+// //this function remove the books from library
+//     btns = document.querySelector('.removeBtn')
+//     btns.forEach(btn => btn.addEventListener('click', () => {console.log("hello")}))
+
+//this is the construct for the books, all our books need to becreated from here
 function Book(title, author, pages, read){
     this.title = title;
     this.author = author;
@@ -55,5 +76,3 @@ function Book(title, author, pages, read){
         return   `${title} by ${author}, ${pages} pages, ${r}`
     }
 }
-
-const book1 = new Book("lo hobbit" , "dostoiewski" , 345 , true);
